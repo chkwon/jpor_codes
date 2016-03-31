20cdfe1ef7b62b5ce081f1ef2ec643865573ef0e
Changhyun Kwon <chkwon@usf.edu>
2016-03-30 23:52:00 -0400

    line search

--- chap6/linesearch.jl
+++ chap6/linesearch.jl
@@ -1,6 +1,6 @@
 using Optim
 
-function f_line(lambda)
+function f_line(x_bar, d_bar, lambda)
     x_new = x_bar + lambda*d_bar
     return (2x_new[1] - 3)^4 + (3x_new[1] - x_new[2])^2
 end
@@ -8,7 +8,8 @@ end
 x_bar = [2.0, 3.0]
 d_bar = [-1.0, 0.0]
 
-opt = optimize(f_line, 0.0, 1.0, method=:golden_section)
+opt = optimize( lambda -> f_line(x_bar, d_bar, lambda),
+                             0.0, 1.0, method=:golden_section)
 
 println("optimal lambda = ", opt.minimum)
 println("optimal f(x+lambda*d) = ", opt.f_minimum)
