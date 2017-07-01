93bcf86e68b991646cb2455abb2eb278338a226a
Changhyun Kwon <chkwon@usf.edu>
2017-07-01 03:53:52 -0400

    LP3.jl error fix

--- chap2/LP3.jl
+++ chap2/LP3.jl
@@ -11,10 +11,10 @@ index_x = 1:3
 index_constraints = 1:2
 
 @variable(m, x[index_x] >= 0)
-@objective(m, Max, sum{ c[i]*x[i], i in index_x} )
+@objective(m, Max, sum( c[i]*x[i] for i in index_x) )
 
 @constraint(m, constraint[j in index_constraints],
-               sum{ A[j,i]*x[i], i in index_x } <= b[j] )
+               sum( A[j,i]*x[i] for i in index_x ) <= b[j] )
 
 @constraint(m, bound, x[1] <= 10)
 
