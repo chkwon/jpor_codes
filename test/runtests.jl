77956b9f6b2cac1c602c0af38b287974450f90a3
Changhyun Kwon <chkwon@gmail.com>
2017-12-26 00:04:04 -0500

    runtests.jl update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
-Pkg.update()
+# Pkg.update()
+
+isTravis = false
+if length(ARGS)>0
+  if ARGS[1] == "travis"
+    isTravis = true
+    info("Travis is ON. Testing Cbc instead of Gurobi.")
+    ENV["PYTHON"]=""
+  end
+end
+
 
 no_test = Set(["runtests.jl", "mcnf.jl"])
+if isTravis
+    push!(no_test, "convex_jump.jl")
+end
+
 
 org_dir = pwd()
 code_dir = normpath(joinpath(dirname(@__FILE__), ".."))
@@ -8,6 +22,66 @@ current = readdir(code_dir)
 
 fails = Array{Any,1}()
 errors = Array{Any,1}()
+
+
+
+function turn_gurobi(onoff)
+
+    old_string = ""
+    new_string = ""
+
+    if onoff == :on
+        old_string = "Cbc"
+        new_string = "Gurobi"
+    elseif onoff == :off
+        old_string = "Gurobi"
+        new_string = "Cbc"
+    end
+
+    for item in current
+      dirpath = joinpath(code_dir, item)
+      if isdir(dirpath)
+        sub = readdir(dirpath)
+
+        for file in sub
+          filepath = joinpath(dirpath, file)
+          if isfile(filepath) && file[end-1:end] == "jl" && file != "runtests.jl"
+
+              f = open(filepath)
+              all_s = readstring(f)
+              close(f)
+
+              if contains(all_s, old_string)
+                  all_s = replace(all_s, old_string, new_string)
+                  open(filepath, "w") do cbc_f
+                      write(cbc_f, all_s)
+                  end
+              end
+
+          end
+        end
+      end
+    end
+
+end
+
+if length(ARGS)>0
+  if ARGS[1] == "gurobi_on"
+      turn_gurobi(:on)
+      error("Gurobi turned on. Test did not run.")
+  end
+end
+
+if isTravis
+    turn_gurobi(:off)
+else
+    turn_gurobi(:on)
+end
+
+
+
+
+
 
 for item in current
   dirpath = joinpath(code_dir, item)
@@ -69,6 +143,15 @@ for item in current
 end
 
 cd(org_dir)
+
+
+
+
+
+if isTravis
+    turn_gurobi(:on)
+end
+
 
 
 for i in 1:length(fails)
