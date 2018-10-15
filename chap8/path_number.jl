6a59cb8630981f510cc2b1a784c8153333500427
Changhyun Kwon <chkwon@usf.edu>
2018-10-15 14:28:01 -0400

    chap 8 // monte carlo ready.

--- chap8/path_number.jl
+++ chap8/path_number.jl
@@ -11,14 +11,13 @@ origin = 1
 destination = 5
 
 N = 100
-estimate = 0
+estimate_samples = Array{Float64}(undef, N)
 for i in 1:N
   I, g = generate_sample_path(adj_mtx, origin, destination)
-  estimate += I / g
+  estimate_samples[i] = I / g
 end
-estimate = estimate / N
-
-println("estimate = ", estimate)
+est = sum(estimate_samples) / N
+println("estimate = ", est)
 
 adj_mtx = [
 0 1 1 0 0 1 0 0 1 0 0 0 0 0 0 0 ;
@@ -42,11 +41,10 @@ origin = 1
 destination = size(adj_mtx,1)
 
 N = 1000
-estimate = 0
+estimate_samples = Array{Float64}(undef, N)
 for i in 1:N
   I, g = generate_sample_path(adj_mtx, origin, destination)
-  estimate += I / g
+  estimate_samples[i] = I / g
 end
-estimate = estimate / N
-
-println("estimate = ", estimate)
+est = sum(estimate_samples) / N
+println("estimate = ", est)
