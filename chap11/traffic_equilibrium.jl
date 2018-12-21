9125023a0311dd336a57b55d58aed993d2b1f827
Changhyun Kwon <chkwon@usf.edu>
2018-12-21 12:52:33 -0500

    adding three new chapters //  complementarity done.

---
+++ chap11/traffic_equilibrium.jl
@@ -0,0 +1,34 @@
+using Complementarity, JuMP
+
+arcs = 1:5
+paths = 1:3
+
+delta = [ 1 0 0 ;
+          0 1 1 ;
+          0 1 0 ;
+          1 1 0 ;
+          0 0 1 ]
+A = [25, 25, 75, 25, 25]
+B = [0.01, 0.01, 0.001, 0.01, 0.01]
+
+Q = 100
+
+m = MCPModel()
+
+@variable(m, h[paths] >= 0)
+@variable(m, u >= 0)
+
+@mapping(m, excess_cost[p in paths],
+    sum(delta[a,p]*(A[a] + B[a]*(sum(delta[a,pp]*h[pp] for pp in paths))^2)
+        for a in arcs) - u )
+@mapping(m, excess_demand, sum(h[p] for p in paths) - Q)
+
+@complementarity(m, excess_cost, h)
+@complementarity(m, excess_demand, u)
+
+status = solveMCP(m)
+
+@show getvalue(h)
+@show getvalue(u)
+@show getvalue(excess_cost)
+@show getvalue(excess_demand)
---
+++ chap11/transmcp.jl
@@ -0,0 +1,40 @@
+using complementarity, JuMP
+
+plants = ["seattle", "san-diego"]
+markets = ["new-york", "chicago", "topeka"]
+
+capacity = [350, 600]
+a = Dict(zip(plants, capacity))
+
+demand = [325, 300, 275]
+b = Dict(zip(markets, demand))
+
+distance = [ 2.5 1.7 1.8 ;
+             2.5 1.8 1.4  ]
+d = Dict()
+for i in 1:length(plants), j in 1:length(markets)
+    d[plants[i], markets[j]] = distance[i,j]
+end
+
+f = 90
+
+m = MCPModel()
+@variable(m, w[i in plants] >= 0)
+@variable(m, p[j in markets] >= 0)
+@variable(m, x[i in plants, j in markets] >= 0)
+
+@NLexpression(m, c[i in plants, j in markets], f * d[i,j] / 1000)
+
+@mapping(m, profit[i in plants, j in markets], w[i] + c[i,j] - p[j])
+@mapping(m, supply[i in plants], a[i] - sum(x[i,j] for j in markets))
+@mapping(m, fxdemand[j in markets], sum(x[i,j] for i in plants) - b[j])
+
+@complementarity(m, profit, x)
+@complementarity(m, supply, w)
+@complementarity(m, fxdemand, p)
+
+status = solveMCP(m)
+
+@show getvalue(x)
+@show getvalue(w)
+@show getvalue(p)
